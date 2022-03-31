a5497cc5da6780b66379154d3afbd0d2126f2f8d
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-03-31 15:16:43 -0400

    Updated slide animation
    Slide animations added using GSAP.
    Uploading to work on further at home.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-const hikeExp = document.querySelector("hike-exp");
+let controller;
+let slideScene;
 
-let observer = new IntersectionObserver(fn, options)
 
-let options = {
-    threshold: 0.5
-}
+// Slides that occur throughout the page. Primarily from left to right concerning images and text.
+function animateSlides() {
+    //Init Controller
+    controller = new ScrollMagic.Controller();
+    //Selct some things
+    const sliders = document.querySelectorAll('.slide')
+    const nav = document.querySelector('.nav-header');
+    // Loop over each slide
+    sliders.forEach(slide =>{
+        const revealImg = slide.querySelector('.reveal-img');
+        const img = slide.querySelector('img');
+        const revealText = slide.querySelector('.reveal-text');
+        // GSAP
+        gsap.to(revealImg, 1, { x: "100%"});
+        const slideTl = gsap.timeline({defaults: {duration: 1, ease: 'power2.inOut'}});
+        slideTl.fromTo(revealImg, {x: '0%'}, {x: '100%'});
+        slideTl.fromTo(img, {scale: 2}, {scale: 1}, '-=1');
+        // after the two objects, the -=1 makes the animation occur 1 second sooner for a simultaneous effect.
+        slideTl.fromTo(revealText, {x: '0%'}, {x:'100%'}, '-=0.75');
+        slideTl.fromTo(nav, {y: '-100%'}, {y:'0%'}, '=-0.5');
+    }
+)};
 
-let observer = interpersonal(fn, options)
-
-observer.observe(slide)
-
-observer.observe(slide)
+animateSlides()
