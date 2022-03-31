12038b3e89695be5deac7365946c0481c0bfc4ed
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-03-30 23:07:57 -0400

    Trio Updated
    Trinity Added.

---
+++ script.js
@@ -0,0 +1,13 @@
+const hikeExp = document.querySelector("hike-exp");
+
+let observer = new IntersectionObserver(fn, options)
+
+let options = {
+    threshold: 0.5
+}
+
+let observer = interpersonal(fn, options)
+
+observer.observe(slide)
+
+observer.observe(slide)
